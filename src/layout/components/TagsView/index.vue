540958ea9959808aa1ceb9b92ec80f927070f8cc
jingshanccc <jingshanccc@qq.com>
2021-01-23 23:15:16 +0800

    [fix] #44
    Close #44

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -4,7 +4,7 @@
       <router-link v-for="tag in visitedViews" ref="tag" :key="tag.path" :class="isActive(tag)?'active':''" :to="{path: tag.path, query: tag.query, fullPath: tag.fullPath}" tag="span" class="tags-view-item" @click.middle.native="closeSelectedTag(tag)" @contextmenu.prevent.native="openMenu(tag, $event)">
         {{ tag.title }}
         <!-- 如果不是首页 则显示关闭按钮 -->
-        <span v-if="tag.name && !tag.name === 'Dashboard'" class="el-icon-close" @click.prevent.stop="closeSelectedTag(tag)" />
+        <span v-if="tag.name && !(tag.name === 'Dashboard')" class="el-icon-close" @click.prevent.stop="closeSelectedTag(tag)" />
       </router-link>
     </scroll-pane>
     <ul v-show="visible" :style="{left: left+'px', top: top+'px'}" class="contextmenu">
